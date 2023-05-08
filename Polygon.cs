a2f2f892fc03fa5a03b0710b9616032a782ccd87
Soundarya777 <128837977+Soundarya777@users.noreply.github.com>
2023-05-08 21:24:09 +0530

    Add files via upload

---
+++ Polygon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_task__Geometry1_
+{
+    /// <summary>
+    /// Polygon collection data
+    /// </summary>
+    public class Polygons
+    {
+        public List<Polygon> polygon = new List<Polygon>();
+    }
+
+    public class Polygon
+    {
+        public List<Line> line = new List<Line>();
+    }
+
+    public class Line
+    {
+        public PointF Startpoint { get; set; }
+        public PointF Endpoint { get; set; }
+        //public Line()
+        //{
+        //    Startpoint = new PointF();
+        //    Endpoint = new PointF();
+        // }
+    }
+
+    public class PolygonsList
+    {
+        public List<LinesList> polygonList = new List<LinesList>();
+    }
+    public class LinesList
+    {
+        public List<PointF> points_list = new List<PointF>();
+    }
+}
+
